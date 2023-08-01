c593c90146e2b9309718f2249c6bbf0c06666637
Malcolm Abbott <mjacoding@gmail.com>
2023-08-01 17:00:35 +0000

    Add a 'submit' event listener to the form element and all of its callback function specifications. #1

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@ const $inputPhoto = document.querySelector('#photo-url');
 const $img = document.querySelector('img');
 
 function inputHandler(event) {
-  const $inputPhotoValue = $inputPhoto.value;
-  $img.setAttribute('src', $inputPhotoValue);
+  const photo = $inputPhoto.value;
+  $img.setAttribute('src', photo);
 }
 
 $inputPhoto.addEventListener('input', inputHandler);
@@ -11,6 +11,28 @@ $inputPhoto.addEventListener('input', inputHandler);
 const $form = document.querySelector('form');
 $form.addEventListener('submit', formHandler);
 
+const $inputTitle = document.querySelector('#title');
+
 function formHandler(event) {
   event.preventDefault();
+  const title = $inputTitle.value;
+  const photo = $inputPhoto.value;
+  const values = {
+    title,
+    photo,
+  };
+  values.entryId = data.nextEntryId;
+  values.entryId++;
+  data[0] = values;
+  $inputPhoto.setAttribute('src', '../images/placeholder-image-square.jpg');
+  $form.reset();
 }
+
+const title = $inputTitle.value;
+const photo = $inputPhoto.value;
+const values = {
+  title,
+  photo,
+};
+values.entryId = data.nextEntryId;
+values.entryId++;
